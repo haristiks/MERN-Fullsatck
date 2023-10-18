6a2cf8e1ff2611f1a4837810dada816b3fabe518
haristiks <nklharis@gmail.com>
2023-10-18 18:14:57 +0530

    changes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 function Register() {
+    
   return (
     <div className='container'>
         <form action="">
